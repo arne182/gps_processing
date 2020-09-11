96013292203b984600face273c3ff9848307b5b9
Arne Schwarck <arneschwarck@gmail.com>
2020-09-11 18:22:33 +0200

    use 55cm accuracy to reduce the data set

--- gps.py
+++ gps.py
@@ -1,5 +1,7 @@
 folder = 'l:\\gps'
 import gpxpy.gpx as gpx
+from rdp import rdp
+import numpy as np
 import datetime
 import os
 def writetogpx(file):
@@ -14,14 +16,21 @@ def writetogpx(file):
         content2.append(eval(x))
       except ValueError:
         continue
+    gps_acc = []
+    for x in content2:
+      if x[5] < 2.0:
+        gps_acc.append(x)
+    gps = [x[2:4] for x in gps_acc]
+    gps_acc = np.array(gps_acc)
+    gps_simple_mask = rdp(gps, epsilon=5e-6, return_mask=True)
+    gps_acc = gps_acc[gps_simple_mask]
     gpx2 = gpx.GPX()
     gpx_track2 = gpx.GPXTrack()
     gpx2.tracks.append(gpx_track2)
     gpx_segment2 = gpx.GPXTrackSegment()
     gpx_track2.segments.append(gpx_segment2)
-    for x in content2:
-      if x[5] < 2.0:
-        gpx_segment2.points.append(gpx.GPXTrackPoint(x[2], x[3], elevation=x[4], time=datetime.datetime.utcfromtimestamp(x[6])))
+    for x in gps_acc:
+      gpx_segment2.points.append(gpx.GPXTrackPoint(x[2], x[3], elevation=x[4], time=datetime.datetime.utcfromtimestamp(x[6])))
     with open(file+'.gpx', "w") as f:
       f.write(gpx2.to_xml())
   except:

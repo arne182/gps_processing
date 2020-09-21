c2aaa623566799833ec9c5a90e5d0471797f595a
Arne Schwarck <arneschwarck@gmail.com>
2020-09-21 11:44:09 +0200

    Add initial sql database functionality

--- gps.py
+++ gps.py
@@ -3,9 +3,36 @@ import gpxpy.gpx as gpx
 from rdp import rdp
 import numpy as np
 import datetime
+import sqlite3
 import gzip
 import os
-def writetogpx(f1, file):
+
+def connectdatabase():
+  conn = sqlite3.connect('L:\\reduced.db')
+  c = conn.cursor()
+  c.execute("""CREATE TABLE IF NOT EXISTS gps(
+               time DECIMAL(12,8),
+               lat DECIMAL(3,15),
+               lon DECIMAL(3,15),
+               altitude DECIMAL(5,15),
+               bearing DECIMAL(5,15),
+               speed DECIMAL(3,15),
+               accuracy DECIMAL(3,15),
+               osm_way_id DECIMAL(12,0),
+               PRIMARY KEY(time, lat, lon))""")
+  conn.commit()
+  return conn
+
+def writedatabase(conn, time, lat, lon, altitude, bearing, speed, accuracy, osm_way_id):
+  c = conn.cursor()
+  try:
+    c.execute("INSERT INTO gps VALUES(" + time + "," + lat + "," + lon + "," + altitude + "," + bearing + "," + speed + "," + accuracy + "," + osm_way_id + ")")
+    conn.commit()
+  except:
+    pass
+  return
+  
+def writetogpx(f1, file, conn):
   try:
     content = [str(x.strip()) for x in f1]
     content = [i for i in content if i.count("[") == 1 and i.count("]") == 1]
@@ -27,6 +54,12 @@ def writetogpx(f1, file):
     gps_acc = np.array(gps_acc)
     gps_simple_mask = rdp(gps, epsilon=5e-6, return_mask=True)
     gps_acc = gps_acc[gps_simple_mask]
+    for x in gps_acc:
+      if len(x) = 8:
+        osm = x[7]
+      else:
+        osm = 0
+      writedatabase(conn,x[6],x[2],x[3],x[4],x[1],x[0],x[5],osm)
     gpx2 = gpx.GPX()
     gpx_track2 = gpx.GPXTrack()
     gpx2.tracks.append(gpx_track2)
@@ -39,6 +72,7 @@ def writetogpx(f1, file):
   except:
     pass
 
+conn = connectdatabase()
 for root, subFolders, files in os.walk(folder):
   for file in files:
     if file.endswith(".gpx"):
@@ -60,5 +94,5 @@ for root, subFolders, files in os.walk(folder):
       except UnicodeDecodeError:
         f1.pop()
       path = os.path.join(root,file)
-    writetogpx(f1, path) 
-
+    writetogpx(f1, path, conn) 
+conn.close()
